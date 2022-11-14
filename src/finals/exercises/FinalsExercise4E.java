e21ecd776d3f7646f86c086774488a2e393cff06
INSTRUCT-D522lab <ioripimentel64@gmail.com>
2022-11-14 13:18:26 +0800

    fixed finals 4d-4f

--- src/finals/exercises/FinalsExercise4E.java
+++ src/finals/exercises/FinalsExercise4E.java
@@ -21,7 +21,7 @@ public class FinalsExercise4E {
   public static void main(String[] args) {
     showIntroduction();
     int arrayLen = getArrayLen();
-    int[] unsortedNames = getNamesList(arrayLen);
+    String[] unsortedNames = getNamesList(arrayLen);
     String[] sortedNames = bubbleSort(unsortedNames);
     printOutput(unsortedNames, sortedNames);
   }
@@ -42,7 +42,7 @@ public class FinalsExercise4E {
   public static String[] getNamesList(int arrayLen) {
     Scanner keyboard = new Scanner(System.in);
     String[] unsortedNames = new String[arrayLen];
-    System.out.println("Numbers to sort:");
+    System.out.println("Names to sort:");
     for (int i = 0; i < arrayLen; i++) {
       System.out.printf("[%d]\t: ", i + 1);
       unsortedNames[i] = keyboard.nextLine();
@@ -73,13 +73,13 @@ public class FinalsExercise4E {
     System.out.println(); // Print unsorted Strings
     System.out.print("Unsorted Names: ");
     for (int i = 0; i < sortedNames.length; i++) {
-      System.out.print(unsortedNames[i] + "\t");
+      System.out.printf("%-6s\t", unsortedNames[i]);
     }
 
     System.out.println(); // Print Strings in lexicographical order
     System.out.print("  Sorted Names: ");
     for (int i = 0; i < sortedNames.length; i++) {
-      System.out.print(sortedNames[i] + "\t");
+      System.out.printf("%-6s\t", sortedNames[i]);
     }
   }
 }
--- src/finals/exercises/FinalsExercise4F.java
+++ src/finals/exercises/FinalsExercise4F.java
@@ -21,7 +21,7 @@ public class FinalsExercise4F {
   public static void main(String[] args) {
     showIntroduction();
     int arrayLen = getArrayLen();
-    int[] unsortedNames = getNamesList(arrayLen);
+    String[] unsortedNames = getNamesList(arrayLen);
     String[] sortedNames = selectionSort(unsortedNames);
     printOutput(unsortedNames, sortedNames);
   }
@@ -42,7 +42,7 @@ public class FinalsExercise4F {
   public static String[] getNamesList(int arrayLen) {
     Scanner keyboard = new Scanner(System.in);
     String[] unsortedNames = new String[arrayLen];
-    System.out.println("Numbers to sort:");
+    System.out.println("Names to sort:");
     for (int i = 0; i < arrayLen; i++) {
       System.out.printf("[%d]\t: ", i + 1);
       unsortedNames[i] = keyboard.nextLine();
@@ -51,7 +51,7 @@ public class FinalsExercise4F {
   }
 
   public static String[] selectionSort(String[] unsortedNames) {
-    String[] sortedNames = new String[5];
+    String[] sortedNames = new String[unsortedNames.length];
 
     for (int i = 0; i < unsortedNames.length; i++) {
       sortedNames[i] = unsortedNames[i]; // Copy unsorted array to new array
@@ -75,13 +75,13 @@ public class FinalsExercise4F {
     System.out.println(); // Print unsorted Strings
     System.out.print("Unsorted Names: ");
     for (int i = 0; i < sortedNames.length; i++) {
-      System.out.print(unsortedNames[i] + "\t");
+      System.out.printf("%-6s\t", unsortedNames[i]);
     }
 
     System.out.println(); // Print Strings in lexicographical order
     System.out.print("  Sorted Names: ");
     for (int i = 0; i < sortedNames.length; i++) {
-      System.out.print(sortedNames[i] + "\t");
+      System.out.printf("%-6s\t", sortedNames[i]);
     }
   }
 }
